17ca67f6928e616c3659c7ee64bde77faa5b6989
SteveMuiyuro <stevemuiyuro@gmail.com>
2024-10-18 02:01:34 +0300

    Add initilization for static files

--- app.py
+++ app.py
@@ -311,4 +311,4 @@ def get_product_prices():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
     app.run(host='0.0.0.0', port=port)
-app = Flask(__name__, static_folder='static')
+
