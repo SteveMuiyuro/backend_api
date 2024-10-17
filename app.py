f193445aae541e071c8ddd969bd1a6ca932b9ee5
SteveMuiyuro <stevemuiyuro@gmail.com>
2024-10-18 02:00:35 +0300

    Add initilization for static files

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from flask_caching import Cache
 import validators
 from flask_cors import CORS
 
+app = Flask(__name__, static_folder='static')
 app = Flask(__name__)
 CORS(app)
 
@@ -310,3 +311,4 @@ def get_product_prices():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
     app.run(host='0.0.0.0', port=port)
+app = Flask(__name__, static_folder='static')
