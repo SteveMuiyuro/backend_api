b82c2726670656840e52efd69bba1c14793a7b51
SteveMuiyuro <stevemuiyuro@gmail.com>
2024-10-17 21:04:14 +0300

    retsructure folder

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,12 @@
+import App from "../../src/App.tsx";
+import "./index.css";
 
-import App from './App.tsx'
-import './index.css'
-
-import React from 'react';
-import ReactDOM from 'react-dom'; // Use the main react-dom import
-
+import React from "react";
+import ReactDOM from "react-dom"; // Use the main react-dom import
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root') // This targets your root element
+  document.getElementById("root") // This targets your root element
 );
