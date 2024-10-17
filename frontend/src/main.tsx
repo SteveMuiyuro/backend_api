d5e11b55034be10c611ebc0d9388afac7f43e905
SteveMuiyuro <stevemuiyuro@gmail.com>
2024-10-17 19:45:28 +0300

    Add build

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';  // This should be 'react-dom/client'
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 
 
